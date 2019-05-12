3ce74c439bb0e53f19b09b11e4588b6a9604b420
HarshitChhipa <hchippa@expedien.in>
2019-05-13 00:13:59 +0530

    *Adding basic functionality like adding friends writing from scratch.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            friendsList: [
+                {
+                    id: 1,
+                    friendsName: 'Jai'
+                },
+                {
+                    id: 2,
+                    friendsName: 'Prachi'
+                },
+                {
+                    id: 3,
+                    friendsName: 'Kirti'
+                },
+                {
+                    id: 4,
+                    friendsName: 'Shubham'
+                }
+            ]
+        };
+
+        this.handelChange = this.handelChange.bind(this);
+
+    }
+
+    handelChange() {
+
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="container">
+                    <input
+                        type="text"
+                        className="my-4 form-control"
+                        placeholder="Type the name of a friend"
+                        onChange={this.handelChange}
+                    />
+                    <ul className="list-group">
+                        {this.state.friendsList.map((item) => {
+                            return <li key={item.id} className="list-group-item">{item.friendsName}</li>
+                        })}
+                    </ul>
+
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
